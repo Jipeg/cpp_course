a8f60f61bf5946564260ba2d915bfcc307443125
Nick <nickandronick@gmail.com>
2022-09-26 15:12:00 +0300

    first commit

---
+++ 1.alg_prog-intro/5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int main() {
+
+    cout << sizeof(short int) << ' ' << sizeof(short int) << "\n";
+    cout << CHAR_MIN;
+    return 0;
+}
